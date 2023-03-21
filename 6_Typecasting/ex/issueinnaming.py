fe639143450f33020180b7049eb850c1c188c5f4
Parameshwar Arunachalam <stark20236@gmail.com>
2023-03-21 06:01:41 +0530

    Fixed ex in chapter 5

--- 6_Typecasting/ex/issueinnaming.py
+++ 6_Typecasting/ex/issueinnaming.py
@@ -6,6 +6,6 @@
 # ex. Tata01, Tata02, Tata03 ....
 
 account_name = input("Enter the name of the account: ")
-instance_id = input("Enter the Id of the machine: ")
+instance_id = int(input("Enter the Id of the machine: "))
 
 print(account_name+instance_id)
