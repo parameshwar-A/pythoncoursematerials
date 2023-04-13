c16746ebaea8b625c619bb263e89252a09aae5f7
Parameshwar Arunachalam <stark20236@gmail.com>
2023-04-13 08:11:22 +0530

    Strings content added

---
+++ 10_Working_with_strings/second_set.py
@@ -0,0 +1,83 @@
+data="vijay"
+
+#print("#"*20)
+#print(data.center(4))
+#print("#"*20)
+
+#print("parameshwar ".endswith("war"))
+#print("parameshwar ".startswith("par"))
+#
+#
+#print("paramesh\tvijay".expandtabs(16))
+
+#print("Hello {1} and welcome {0}".format("vijay","paramesh"))
+
+#print("-".join(["1","2","3"]))
+
+
+#print(data.ljust(20))
+#print(data.ljust(20,"$"))
+
+filled_data="$$$$$vijay$$$$$"
+#print(filled_data.lstrip("v"))
+#print("www.gmail.com".lstrip("w.ga"))
+#
+#
+#print("welcome".maketrans("abcdefgh","hgfedcba"))
+#
+#print("welwisher".translate("welcome".maketrans("welcome","seclome")))
+#
+#print("AmIAGoodGuy".removeprefix("Am"))
+#print("AmIAGoodGuy".removesuffix("Guy"))
+
+#print("welcomewellwisher".replace('w','s',1))
+
+#welcomewellwisher
+#0123456789
+#print("welcomewellwisher".find("wel"))
+#print("welcomewellwisher".rfind("wel"))
+#
+#print("welcomewellwisher".rindex("e"))
+
+#print(data.rjust(60))
+#print(data.rjust(20,"$"))
+#
+#print("welcomewell".rpartition('e'))
+
+
+#print("welcomewell".rsplit("e",1))
+#print("welcomewell".split("e",1))
+
+
+para='''
+Welcome to the list of lines
+This is the first line
+This is the second line
+This is the third line
+'''
+
+print(para.splitlines(True))
+
+
+
+# casefold 
+#print("casefold: ", "ß".casefold())
+#print("lower: ", "ß".lower())
+#print("casefold: ", "s".casefold())
+#print("lower: ", "s".lower())
+#
+# Encoding is for storing and accessing data
+#tam_sent = "வணக்கம்"
+#
+#print(tam_sent.encode(encoding="utf-8"))
+#print(tam_sent.encode(encoding="utf-16"))
+
+#strip, zfill is pending
+print('www.example.com'.strip('cmowz.'))
+
+
+print("+420".zfill(5))
+                  #
+                  #
+                  #
+                  
--- 12_Loops/for_loops.py
+++ 12_Loops/for_loops.py
@@ -1,8 +1,17 @@
 # For Loops
 # used when exact number of iteration is determined the start and stop
-
-#for num in range(0,10):
+numbers=[1,2,3,4,5,6,7,8,9,10]
+#for num in range(100, 0, -1 ):
 #    print(num)
+
+#print("parameshwar"[::20])
+#print(list(range(1,11)))
+
+name_list=["nihaal","dhana", "paramesh", "ayaanar", "guhan"]
+
+for letter in range(97,123):
+    lower=chr(letter)
+    print(lower, lower.upper())
 
 # Let's take a deep look
 
@@ -15,5 +24,5 @@
 # The range function also has a step value just like string slicing syntax
 
 #range(start, stop, step) Default step size is 1
-for num in range(0,10,2):
-    print(num)
+#for num in range(0,10,2):
+#    print(num)
