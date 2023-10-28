5be42017523a2fbea583c3568720fda68c8db65c
Parameshwar Arunachalam <stark20236@gmail.com>
2023-10-28 08:05:49 +0530

    revising python content template is added

---
+++ revision.py
@@ -0,0 +1,150 @@
+################## Python revisions ##################
+
+#### math operations ####
+
+# add
+
+# subtraction
+
+# division
+
+# multiplication
+
+# Getting reminder 
+
+# Exponential
+
+#### Basic data type ####
+
+# integers
+
+# float
+
+# string
+
+# boolean
+
+#### type casting ####
+
+# Int conversions
+
+# int -> float
+
+# int -> string
+
+# int -> boolean
+
+# float conversions
+
+# float -> int (but data loss will be there)
+
+# float - > string
+
+# float -> boolean
+
+# string conversions
+
+# string -> int
+
+# string -> float
+
+# string -> boolean
+
+# boolean conversions 
+
+# bool -> int (always be 1 or 0)
+
+# bool -> float (always be 1.0 or 0.0)
+
+# bool -> string (True or false as a string) 
+
+
+#### Operators #### 
+
+# comparative
+
+# greater than / greater than equals to
+
+# lesser than / lesser than equal to 
+
+# equals to
+
+# not equals to 
+
+# Logical 
+
+# and 
+
+# or 
+
+# not
+
+# Assignment operators
+
+# = for regular purpose
+
+# += 
+
+# #= 
+
+# *=
+
+# /=
+
+####  variable rules ####
+
+# Name should start with alphabet or _
+# It can contains number (0-9) but you cannot start a name with numbers
+# It should only conains alphabets(a-z A-Z), numbers(0-9) or _
+# Variables are case sensitive (pi_vaue and Pi_value are different)
+# It should not be existing reserved name of the python language
+
+
+#### Getting help ####    
+
+# use help() func inside python
+# use pydoc in terminal
+# use docs in internet
+
+#### String #### 
+
+# use either '' or ""
+# Both are exactly same
+
+# They have associated methods
+# You can access them with . operator
+
+# lower
+
+# upper
+
+# index
+
+# list all associated methods with dir command
+
+#### Control structures ####
+
+# if 
+
+# if else
+
+# if else ladder
+
+# nested if else
+
+#### loops ####
+
+# for loop (when limit is defined)
+
+# while loop ( when success is defined)
+
+#### Data structure ####
+
+# list
+
+# tuple
+
+# set 
+
+# dictionary
+
